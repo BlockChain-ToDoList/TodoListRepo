e7e94dbd9dccc31c4ac271b73d9dd279723f1529
Owen <onskys005@gmail.com>
2024-04-25 20:31:54 -0700

    Added a Solidity event

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -11,13 +11,21 @@ contract TodoList {
 
     mapping(uint => Task) public tasks; 
 
+    // This is a Solidity event.
+    event TaskCreated(
+        uint id, 
+        string content,
+        bool completed
+    );
+
     constructor() public {
-        createTask("Check out dappuniversity.com"); 
+        createTask("Your to-do list."); 
     }
 
     function createTask(string memory _content) public {
         taskCount ++;
         tasks[taskCount] = Task(taskCount, _content, false); 
+        emit TaskCreated(taskCount, _content, false)
     }
 
 
